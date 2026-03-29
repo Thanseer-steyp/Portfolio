7883504e25efe8a45cc6d178244ccfe8cd08644b
Thanseer-steyp <thanseersteyp@gmail.com>
2026-03-29 22:55:05 +0530

    Logo name removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,10 +54,10 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+          <img src={logo} alt="logo" className="w-14 h-14 object-contain" />
+          {/* <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Thanseer &nbsp;
-          </p>
+          </p> */}
         </Link>
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
